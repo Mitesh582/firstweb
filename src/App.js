d9162964efe688bef939e0475843583db1959ff8
Mitesh582 <miteshjethava001@gmail.com>
2022-11-18 15:22:40 +0530

    Hello

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */
+      }
+      
+      <h1>Hello Wolrd!</h1>
+      <h2>I Am Mitesh Jethava</h2>
+      <p> I am fron-end-devloper</p>
+    </div>
+  );
+}
+
+export default App;
