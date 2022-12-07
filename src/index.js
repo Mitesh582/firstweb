c5af28c9b4cfabb75306c10e770060b8daf68166
Mitesh582 <miteshjethava001@gmail.com>
2022-12-07 15:42:30 +0530

    Header  Commit

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from '../src/componate/App';
-// import Header from '../src/componate/Header';
-// import Counter from './componate/Counter';/
-import Theme from './componate/Theme';
+import Header from '../src/componate/Header';
+// import Counter from './componate/Counter';
+// import Theme from './componate/Theme';
 // import Gallery from './componate/Gallery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const lists = ['Home', 'About', 'Service', 'Pages', 'Contact-Us'];
+const lists = ['Home', 'About', 'Service', 'Pages', 'Contact-Us'];
 root.render(
 
       <>
-      {/* <Header logo='images/logo.png' dropdown='Pages' list = {lists} /> */}
+      <Header logo='images/logo.png' dropdown='Pages' list = {lists} />
       {/* <Header logo='images/logo2.png' dropdown='shop' /> */}
       {/* <Counter/> */}
-      <Theme/>
+      {/* <Theme/> */}
       </>
 
 );
